3089b0df84e123459e7cb517a6a27bc0aa6278b6
landalfyao <11018109+landalfyao@user.noreply.gitee.com>
2023-03-20 03:27:41 +0000

    fix: 修改静态文件配置
    Signed-off-by: landalfyao <11018109+landalfyao@user.noreply.gitee.com>

--- server/src/constant.ts
+++ server/src/constant.ts
@@ -1,58 +1,58 @@
 // app 小程序 登录 凭证信息
-export const CONFIG_APPAUTH_INFO = 'appAuthInfo';
+export const CONFIG_APPAUTH_INFO = 'ddapp-free-appAuthInfo';
 // 地图秘钥
-export const CONFIG_MAPKEY = 'mapKey';
+export const CONFIG_MAPKEY = 'ddapp-free-mapKey';
 // 阿里云配置
-export const CONFIG_ALI = 'aliInfo';
+export const CONFIG_ALI = 'ddapp-free-aliInfo';
 // 优惠券配置
-export const CONFIG_COUPON = 'coupon';
+export const CONFIG_COUPON = 'ddapp-free-coupon';
 // app 小程序 支付配置
-export const CONFIG_APPMCH = 'appMch';
+export const CONFIG_APPMCH = 'ddapp-free-appMch';
 // 通知配置
-export const CONFIG_NOTICE = 'notice';
+export const CONFIG_NOTICE = 'ddapp-free-notice';
 // 取消订单配置
-export const CONFIG_ORDER_CANCEL = 'orderCancel';
+export const CONFIG_ORDER_CANCEL = 'ddapp-free-orderCancel';
 // 订单小费配置
-export const CONFIG_ORDER_FEE = 'orderFee';
+export const CONFIG_ORDER_FEE = 'ddapp-free-orderFee';
 // 订单接单设置
-export const CONFIG_ORDER_RECEIVE = 'orderReceive';
+export const CONFIG_ORDER_RECEIVE = 'ddapp-free-orderReceive';
 // 用户指南
-export const CONFIG_GUIDE_USER = 'guideUser';
+export const CONFIG_GUIDE_USER = 'ddapp-free-guideUser';
 // 骑手指南
-export const CONFIG_GUIDE_RIDER = 'guideRider';
+export const CONFIG_GUIDE_RIDER = 'ddapp-free-guideRider';
 // 骑手协议
-export const CONFIG_AGREEMENT_RIDER = 'riderAgreement';
+export const CONFIG_AGREEMENT_RIDER = 'ddapp-free-riderAgreement';
 // 提现设置
-export const CONFIG_CASH = 'cash';
+export const CONFIG_CASH = 'ddapp-free-cash';
 
-export const CONFIG_SETTING_CASH_SCHOOL = 'cashSettingSchool';
+export const CONFIG_SETTING_CASH_SCHOOL = 'ddapp-free-cashSettingSchool';
 // 分享设置
-export const CONFIG_SHARE = 'share';
+export const CONFIG_SHARE = 'ddapp-free-share';
 // 协议及指南
-export const CONFIG_PROTOCOL_SCHOOL = 'protocolSchool';
-export const HELP_DELIVER = 'helpDepliver';
-export const HELP_GET = 'helpGet';
-export const HELP_BUY = 'helpBuy';
-export const WXAPP_ACCESSTOKEN = 'wxapp_accesstoken';
-export const WXAPP_ACCESSTOKEN_SCHOOL = 'wxapp_accesstoken_school';
-export const CORWX_ACCESSTOKEN = 'corwx_accesstoken';
+export const CONFIG_PROTOCOL_SCHOOL = 'ddapp-free-protocolSchool';
+export const HELP_DELIVER = 'ddapp-free-helpDepliver';
+export const HELP_GET = 'ddapp-free-helpGet';
+export const HELP_BUY = 'ddapp-free-helpBuy';
+export const WXAPP_ACCESSTOKEN = 'ddapp-free-wxapp_accesstoken';
+export const WXAPP_ACCESSTOKEN_SCHOOL = 'ddapp-free-wxapp_accesstoken_school';
+export const CORWX_ACCESSTOKEN = 'ddapp-free-corwx_accesstoken';
 
 export type DISCOUNT_TYPE = 'coupon' | 'paperRule';
 
 // 微信小程序订阅消息配置
-export const WXAPP_SUBSCRIBE_SCHOOL = 'wxappSubscribeSchool';
+export const WXAPP_SUBSCRIBE_SCHOOL = 'ddapp-free-wxappSubscribeSchool';
 
-export const PLUGINS_ACTIVITY_SETTING_SCHOOL = 'pluginsActivitySettingSchool';
+export const PLUGINS_ACTIVITY_SETTING_SCHOOL = 'ddapp-free-pluginsActivitySettingSchool';
 
-export const SYSTEM_SETTING = 'systemSetting';
-export const MINIAPP_SCHOOL = 'miniapp_school';
-export const MINIAPP_CITY = 'miniapp_city';
+export const SYSTEM_SETTING = 'ddapp-free-systemSetting';
+export const MINIAPP_SCHOOL = 'ddapp-free-miniapp_school';
+export const MINIAPP_CITY = 'ddapp-free-miniapp_city';
 
-export const CONFIG_ORDERTYPE_SCHOOL = 'configOrdertypeSchool';
+export const CONFIG_ORDERTYPE_SCHOOL = 'ddapp-free-configOrdertypeSchool';
 
 // 时间要求配置
-export const CONFIG_TIME_REQUIREMENTS_SCHOOL = 'configTimeRequirementsSchool';
+export const CONFIG_TIME_REQUIREMENTS_SCHOOL = 'ddapp-free-configTimeRequirementsSchool';
 
-export const CONFIG_PRINT = 'configPrint';
-export const SCHOOL_INFO = 'schoolInfo';
-export const SCHOOL_SERVICE_DATA = 'schoolServiceData';
+export const CONFIG_PRINT = 'ddapp-free-configPrint';
+export const SCHOOL_INFO = 'ddapp-free-schoolInfo';
+export const SCHOOL_SERVICE_DATA = 'ddapp-free-schoolServiceData';
